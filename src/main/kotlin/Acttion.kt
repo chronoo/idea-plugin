28c56bf3b1939bdcd05f210aa77a96f49dccd9aa
chrono <chrono059@gmail.com>
2022-03-04 23:12:50 +0300

    up

--- src/main/kotlin/Acttion.kt
+++ src/main/kotlin/Acttion.kt
@@ -1,7 +1,6 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.ui.Messages
 
 class Acttion : AnAction() {
