57427b6efa943e1d3c564e7d943ac78a5dc362cb
chrono <chrono059@gmail.com>
2022-03-10 02:38:32 +0300

    refactor

--- src/main/kotlin/NotStepActionsInspection.kt
+++ src/main/kotlin/NotStepActionsInspection.kt
@@ -15,22 +15,20 @@ class NotStepActionsInspection : AbstractBaseJavaLocalInspectionTool() {
         object : JavaElementVisitor() {
             override fun visitMethodCallExpression(expression: PsiMethodCallExpression?) {
                 if (expression?.parentOfType<PsiClass>()?.isTestClass == true) {
-                    val method = expression.resolveMethod()
-                    if (method != null) {
+                    expression.resolveMethod()?.let { method ->
                         if (!AnnotationUtil.isAnnotated(method, STEP_ANNOTATION, 0)) {
                             if (expression is CompositeElement) {
                                 val children = expression.findChildByType(ElementType.REFERENCE_EXPRESSION)
                                 if (children is PsiReferenceExpression) {
-                                    val child = children.children.firstOrNull {
-                                        it.text == method.name && it.elementType == ElementType.IDENTIFIER
-                                    }
-                                    if (child != null) {
-                                        holder.registerProblem(
-                                            child,
-                                            "Вызов не @Step-метода в тесте",
-                                            ProblemHighlightType.WARNING
-                                        )
-                                    }
+                                    children.children
+                                        .firstOrNull { it.text == method.name && it.elementType == ElementType.IDENTIFIER }
+                                        ?.apply {
+                                            holder.registerProblem(
+                                                this,
+                                                "Вызов не @Step-метода в тесте",
+                                                ProblemHighlightType.WARNING
+                                            )
+                                        }
                                 }
                             }
                         }
