8a6e5dc4580ff9777294c6c33420655b994a1270
chrono <chrono059@gmail.com>
2022-03-11 00:11:02 +0300

    not-step inspection fix

--- src/main/kotlin/NotStepActionsInspection.kt
+++ src/main/kotlin/NotStepActionsInspection.kt
@@ -7,33 +7,47 @@ import com.intellij.psi.impl.source.tree.CompositeElement
 import com.intellij.psi.impl.source.tree.ElementType
 import com.intellij.psi.util.elementType
 import com.intellij.psi.util.parentOfType
+import com.intellij.psi.util.parentsOfType
 import common.STEP_ANNOTATION
 import common.isTestClass
+import common.isTestMethod
 
 class NotStepActionsInspection : AbstractBaseJavaLocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor =
         object : JavaElementVisitor() {
             override fun visitMethodCallExpression(expression: PsiMethodCallExpression?) {
                 if (expression?.parentOfType<PsiClass>()?.isTestClass == true) {
-                    expression.resolveMethod()?.let { method ->
-                        if (!AnnotationUtil.isAnnotated(method, STEP_ANNOTATION, 0)) {
-                            if (expression is CompositeElement) {
-                                val children = expression.findChildByType(ElementType.REFERENCE_EXPRESSION)
-                                if (children is PsiReferenceExpression) {
-                                    children.children
-                                        .firstOrNull { it.text == method.name && it.elementType == ElementType.IDENTIFIER }
-                                        ?.apply {
+                    if (expression.parentsOfType<PsiMethod>().firstOrNull()?.isTestMethod == true) {
+                        expression.resolveMethod()?.let { method ->
+                            if (!AnnotationUtil.isAnnotated(method, STEP_ANNOTATION, 0)) {
+                                if (expression is CompositeElement) {
+                                    val children = expression.findChildByType(ElementType.REFERENCE_EXPRESSION)
+                                    if (children is PsiReferenceExpression) {
+                                        val methodsExpressions = expression.parentsOfType<PsiMethodCallExpression>()
+                                            .lastOrNull()
+                                            ?.children?.firstOrNull()
+                                            ?.children?.filter { it.elementType == ElementType.IDENTIFIER }
+
+                                        methodsExpressions?.firstAndLastOrNull()?.forEach { methodExpression ->
                                             holder.registerProblem(
-                                                this,
+                                                methodExpression,
                                                 "Вызов не @Step-метода в тесте",
                                                 ProblemHighlightType.WARNING
                                             )
                                         }
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+        }
+
+    private fun <T> List<T>.firstAndLastOrNull(): List<T>? =
+        when {
+            isEmpty() -> null
+            size == 1 -> listOf(first())
+            else -> listOf(first(), last())
         }
 }
